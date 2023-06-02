6cc5879599c9498ff2b4bb18f928d20ae3bd5a92
Kirill <Preshepkin73@gmail.com>
2023-06-02 03:57:08 +0300

    aad/sections

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+// $( document ).ready(function() {
+//   const tabEl = document.querySelector('button[data-bs-toggle="tab"]')
+//   tabEl.addEventListener('shown.bs.tab', event => {
+//     event.target // newly activated tab
+//     event.relatedTarget // previous active tab
+//   })
+
+// });
