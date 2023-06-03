903d355f50fab2bdd3a655b8c577cc3cbe467135
Kirill <Preshepkin73@gmail.com>
2023-06-03 08:02:20 +0300

    add/script

--- src/js/script.js
+++ src/js/script.js
@@ -8,13 +8,55 @@ $( document ).ready(function() {
 		$('.overlay, #consultation').fadeOut('slow');
 	});
 
-	$('.button_mini').each(function(i) {
-		$(this).on('click', function() {
-			$('#order .madal__descr').text($('.catalog-item__subtitle').eq(i).text());
-			$('.overlay, #order').fadeIn('slow');
-		})
-	});
+	// $('.button_mini').each(function(i) {
+	// 	$(this).on('click', function() {
+	// 		$('#order .madal__descr').text($('.catalog-item__subtitle').eq(i).text());
+	// 		$('.overlay, #order').fadeIn('slow');
+	// 	})
+	// });
 	
+	function validateForms(form){
+        $(form).validate({
+			errorClass: 'onerror',
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя !!!",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                phone: "Пожалуйста, введите свой номер телефона",
+            }
+        });
+    };
+
+    validateForms('#consultation form');
+
+	$('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation').fadeOut();
+            $('.overlay').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+    
 
 	$('input[name=phone]').mask('+7 (999) 999-99-99' );
 
