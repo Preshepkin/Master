868c63e109f989feb031e72ba522a021c68f9296
Kirill <Preshepkin73@gmail.com>
2023-06-03 07:32:20 +0300

    add/script

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,26 @@
-// $( document ).ready(function() {
-//   const tabEl = document.querySelector('button[data-bs-toggle="tab"]')
-//   tabEl.addEventListener('shown.bs.tab', event => {
-//     event.target // newly activated tab
-//     event.relatedTarget // previous active tab
-//   })
+$( document ).ready(function() {
 
-// });
+
+	$('[data-modal=consultation]').on('click', function() {
+		$('.overlay, #consultation').fadeIn('slow');
+	});
+	$('.modal_item_close').on('click', function() {
+		$('.overlay, #consultation').fadeOut('slow');
+	});
+
+	$('.button_mini').each(function(i) {
+		$(this).on('click', function() {
+			$('#order .madal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+		})
+	});
+	
+
+	$('input[name=phone]').mask('+7 (999) 999-99-99' );
+
+
+	new WOW().init(
+		
+		);
+	
+});
